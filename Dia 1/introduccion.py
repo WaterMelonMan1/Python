39e8bcc20f67d52ed52fa1e34063db218fef0911
Alejandro <ralejoospi@gmail.com>
2023-05-26 15:58:39 -0500

    continuo dia1
    se termina el dia 1

--- Dia 1/introduccion.py
+++ Dia 1/introduccion.py
@@ -1,1 +1,11 @@
-print()
+#operador print
+print("Hola Mundo hoy empiezo mi curso de 16 dias o mas de python avanzado")
+print('Asi se pone un texto "Con doble comilla dentro de un print" XD jaja')
+
+# Los string
+print("Hola",'Alejo')
+print("Tambien se puede usar un" + " +")
+print("la barra invertida es para poner signos especiales y que el cpdigo no los tome")
+print("Por ejemplo \"Era una vez\"")
+print("Si le añades la n significa \nNueva linea")
+print("Holis")
