4ca366257348c9e40b8cb64e8070b476c670008b
Alejandro <ralejoospi@gmail.com>
2023-05-30 19:10:24 -0500

    Finalizacion dia 2
    se da por terminado el aprendizaje del dia 2

--- Dia 2/Variables.py
+++ Dia 2/Variables.py
@@ -1,1 +1,12 @@
-Dia2 = "Este es el dia 2"
+#actividad 1
+nombre = "Tony Soprano"
+edad = 51
+
+#Actividad 2
+nombre = "Julia"
+Apellido = " Roberts"
+nombreCompleto = nombre + Apellido
+
+#acitividad 3
+curso = "Python"
+print('Estás tomando un curso de ' + curso)
