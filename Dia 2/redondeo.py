2c3cc90b2d8b3f58a82e5363eecd249ac983500e
Alejandro <ralejoospi@gmail.com>
2023-06-03 17:24:34 -0500

    suma 2
    se agrega el dia2 para estar al dia

---
+++ Dia 2/redondeo.py
@@ -0,0 +1,14 @@
+valor = 95.6666666
+
+print(round(valor,1))
+
+#actividad 1
+numero = round(10/3,2)
+print(numero)
+
+#actividad 2
+valor = round(10.676767)
+print(valor)
+
+#actividad 3
+print(round(5**0.5,4))
